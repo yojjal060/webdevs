421651897b281fb394a90eb210d56eea21cb9a95
yojjal060 <yojjal878@gmail.com>
2024-07-25 00:04:54 +0545

    revised some concepts

---
+++ exams/script.js
@@ -0,0 +1,26 @@
+// let text = `{
+//     "name":"John",
+//     "age":30
+// }`
+
+// let obj = JSON.parse(text);
+
+// document.getElementById("demo").innerHTML = obj.name + "yayay" + obj.age;
+ 
+
+let array = ['csit','it','bca','bim'];
+document.getElementById("demo").innerHTML= array.join(' ');
+
+//hide/show
+// $(document).ready(function() {
+//     $("#demo").click(function() {
+//         $(this).hide(1000);
+//     });
+// });
+
+//fade
+// $(document).ready(function() {
+//     $("#demo").click(function() {
+//         $(this).slideDown(1000);
+//     });
+// });
